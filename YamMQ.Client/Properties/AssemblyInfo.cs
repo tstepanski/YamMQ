59e3b2f6836e5c3638e6a94140c4261a750464a9
Timothy Stepanski <timothy@timstepanski.com>
2016-10-29 17:09:20 -0400

    refactored MQ to use .Net Framework instead of .Net Ccore

--- YamMQ.Client/Properties/AssemblyInfo.cs
+++ YamMQ.Client/Properties/AssemblyInfo.cs
@@ -2,18 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("YamMQ.Client")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Great American Insurance Group")]
+[assembly: AssemblyCompany("Timothy M. Stepanski")]
 [assembly: AssemblyProduct("YamMQ.Client")]
+[assembly: AssemblyCopyright("Copyright © Timothy M. Stepanski 2016")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e72c2a49-0e99-4a47-9da2-43b190a1dd97")]
+[assembly: Guid("f5742af7-191f-4336-8790-d039ca090168")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- YamMQ.General/Properties/AssemblyInfo.cs
+++ YamMQ.General/Properties/AssemblyInfo.cs
@@ -2,18 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("YamMQ.General")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Great American Insurance Group")]
+[assembly: AssemblyCompany("Timothy M. Stepanski")]
 [assembly: AssemblyProduct("YamMQ.General")]
+[assembly: AssemblyCopyright("Copyright © Timothy M. Stepanski 2016")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bd043eb9-5337-40e0-91a8-59dc396d1d91")]
+[assembly: Guid("5cc5e87e-31fe-4a14-9775-4674ba574029")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
